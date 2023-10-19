616237bbbfd277e32ccecbb1bfd5bd3c0b2ce092
ANDRE ABREO <96838895+AndreAbreo@users.noreply.github.com>
2023-10-19 00:05:47 +0000

    commit final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+console.log("Hola");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ext = [".com", ".net", ".us"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function domainGenerator() {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < ext.length+1; m++) {
+          if (m < ext.length){
+            console.log(pronoun[i] + adj[j] + noun[k] + ext[m]);
+          } else {
+            let noun1 = noun[k];
+            console.log(pronoun[i] + adj[j] + (noun1.slice(0, -2)) +"."+ (noun1.slice(-2)));
+          }
+        }
+      }
+    }
+  }
+}
+domainGenerator();
